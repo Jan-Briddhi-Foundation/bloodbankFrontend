3054e953c4116550e61682e8e4d610ebdc25e267
genion <Sulaimontaofik08@gmail.com>
2023-11-02 11:29:29 +0100

    Added new functionality

--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -1,22 +1,23 @@
 import Header from '../../components/Header/Header';
-
+import Caret from '../../assets/CaretCircleRight.svg';
+import style from './Hompage.module.css';
 
 import React from 'react'
 
 function Homepage() {
   return (
-    <>
+    <main>
       <Header />
-      <div>
-        <h1>Home</h1>
-        <p>xxxxx</p>
-            <section>
-                <button>Submit New Request</button>
-                <buuton>View Request History</buuton>
-                <button>Edit My Information</button>
+      <div className={style.Home} >
+        <h1 className={style.title} > Home</h1>
+        <p className={style.text} >xxxxx</p>
+            <section className={style.buttonNav}>
+                <button className={style.button}><p className={style.buttonText}>Submit New Request  </p> <img src={Caret} alt='' className={style.circle} /></button>
+                <button className={style.button}><p className={style.buttonText}>View Request History</p> <img src={Caret} alt='' className={style.circle} /></button>
+                <button className={style.button}><p className={style.buttonText}>Edit My Information </p> <img src={Caret} alt='' className={style.circle} /></button>
             </section>
       </div>
-    </>
+    </main>
   )
 }
 
--- src/pages/NewRequest/NewRequest.js
+++ src/pages/NewRequest/NewRequest.js
@@ -1,27 +1,29 @@
 import Header from '../../components/Header/Header';
 import style from './NewRequest.module.css';  
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
-import React, { useState } from 'react';
+import { useState } from 'react';
 import DatePicker from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker.css';
+import 'react-datepicker/dist/react-datepicker.module.css';
+import './DatePicker.css';
+
 
 
 function NewRequest() {
-    const [selectedDate, setSelectedDate] = useState(null);
+
   return (
     <>
       <Header />
       <div>
-        <h1>Blood Request</h1>
-        <p>Let the people around you help you.</p>
+        <h1 className={style.title}>Blood Request</h1>
+        <p  className={style.title}>Let the people around you help you.</p>
         <section>
-            <input type='text' placeholder='Patient Name' name=''/>
-            <div className={`${style.selectContainer} ${style.first} `}>
-            <select name="bloodType" placeholder='Blood Type' className={` ${style.select}`}>
-              <option value="" selected>
-                Blood Type
+            <input type='text' placeholder='Patient Name' name=''  />
+            <div className={`${style.selectContainer} ${style.first}`}>
+            <select name="bloodType"  className={`${style.select}`}>
+              <option value="" selected className={`${style.selectit}`}>
+                Blood Group
               </option>
-              <option value="A">A</option> 
+              <option value="A" className={`${style.selectit}`}>A</option> 
               <option value="B">B</option>
               <option value="AB">AB</option>
               <option value="O">O</option>
@@ -29,18 +31,42 @@ function NewRequest() {
             <div className={style.IconContainer}>
                 <ArrowDropDownIcon className={style.DropIcon} sx={{ fontSize: 50 }} />
             </div>
-            </div>
-            <DatePicker    
-                 selected={selectedDate}
-                 onChange={(date) => setSelectedDate(date)}
-                 placeholderText="Date for Blood Needed" 
-            />
+            </div>            
             <input type='number' placeholder='Quantity of Blood in Unit' />
-            <input type="tel" placeholder='Phone Number ' />
+            <ReactDatePicker  wrapperClassName="datePicker" />
+            <input type='number' placeholder='Quantity of Blood in Unit' />
+            <input type='Tel' placeholder='Phone Number'  />
+            <input type='text' placeholder='Address' name=''/>
+            <input type='Text' placeholder='Location' />
+
+            <button>Send Request</button>
+
+
+
         </section>
       </div>
     </>
   )
 }
 
+function ReactDatePicker() {
+
+  const [selectedDate, setSelectedDate] = useState(null);
+
+  const handleDateChange = (date) => {
+    setSelectedDate(date);
+    
+  }
+  return (
+    <div>
+      <DatePicker 
+        selected={selectedDate}
+        onChange={handleDateChange}
+        dateFormat="MM/dd/yyyy"
+        placeholderText='Date for Blood Needed'
+        minDate={ new Date() }
+      />
+    </div>
+  )
+}
 export default NewRequest;
