d3765284022cbae198f8812b087697fbb12c134e
taofik <sulaimontaofik08@gmail.com>
2024-01-26 22:56:35 +0000

    made some changes to the password

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -37,7 +37,7 @@ const Login = () => {
       )
     ) {
       toast.error(
-        "Password should contain at least one uppercase, one lowercase, one number, and one special character"
+        "Password should contain letters and numbers only"
       );
       error = true;
     }
