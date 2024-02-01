c87a7eba1acc4f71af85a316a1224b7e5cfe55b8
dotjarvis <johnsonmakings@gmail.com>
2024-02-01 08:13:37 +0300

    BloodrequestHistory confirmed working

--- src/pages/RequestHistory/RequestHistory.js
+++ src/pages/RequestHistory/RequestHistory.js
@@ -7,8 +7,10 @@ import location from "../../assets/location.svg";
 // import { getNotifications } from "../../apis/Notifications.hooks";
 import { useEffect, useState } from "react";
 import { requestHistory } from "../../apis/BloodRequestHistory";
+
 const RequestHistoryPage = () => {
   const [notifications, setNotifications] = useState([]);
+
   const getAllNotifications = async () => {
     const data = await requestHistory();
     setNotifications(data);
@@ -16,10 +18,7 @@ const RequestHistoryPage = () => {
   useEffect(() => {
     getAllNotifications();
   }, []);
-  useEffect(() => {
-    console.log(notifications);
-  });
-  // console.log(notifications);
+
   return (
     <>
       <Header />
@@ -28,44 +27,51 @@ const RequestHistoryPage = () => {
           <h1>Request History</h1>
           <span>Overview of all the requests you've submitted</span>
         </div>
-        {notifications?.length === 0 ? (
-          <div className="mt-4 flex flex-col gap-2 w-[100%] p-4  md:w-[60vw]">
-            <section className="relative rounded-[5px] pl-[2vh] pr-[4vh] py-[2vh] flex justify-between h-[12vh] bg-[rgba(249,_249,_249,_1)] w-[100%]">
-              <div className="w-[full] flex flex-col justify-between gap-2 text-left">
-                <span>Blood Request: Type AB</span>
-                <div className="flex gap-[2rem]">
-                  <span className="flex gap-1 items-center text-xs">
-                    <img
-                      className="w-[1rem] h-[1rem]"
-                      src={calendar}
-                      alt="calendarIcon"
-                    />
-                    Before 05/31/23
+        {notifications.history ? (
+          notifications.history.map((request, index) => (
+            <div
+              key={index}
+              className="mt-4 flex flex-col gap-2 w-[100%] p-4  md:w-[60vw]"
+            >
+              <section className="relative rounded-[5px] pl-[2vh] pr-[4vh] py-[2vh] flex justify-between h-[12vh] bg-[rgba(249,_249,_249,_1)] w-[100%]">
+                <div className="w-[full] flex flex-col justify-between gap-2 text-left">
+                  <span>
+                    Blood Request: Type {notifications.profileForm.bloodGroup}
                   </span>
-                  <span className="flex gap-1 items-center text-xs">
-                    <img
-                      className="w-[1rem] h-[1rem]"
-                      src={location}
-                      alt="calendarIcon"
-                    />
-                    Hospital
-                  </span>
+                  <div className="flex gap-[2rem]">
+                    <span className="flex gap-1 items-center text-xs">
+                      <img
+                        className="w-[1rem] h-[1rem]"
+                        src={calendar}
+                        alt="calendarIcon"
+                      />
+                      Before {request.date_needed}
+                    </span>
+                    <span className="flex gap-1 items-center text-xs">
+                      <img
+                        className="w-[1rem] h-[1rem]"
+                        src={location}
+                        alt="calendarIcon"
+                      />
+                      Hospital
+                    </span>
+                  </div>
                 </div>
-              </div>
-              <div className="flex  self-start items-center">
+                <div className="flex  self-start items-center">
+                  <img
+                    src={share}
+                    alt="share icon"
+                    className="h-[3.125rem] w-[1.875rem] relative -bottom-[0.85rem] -right-[0.85rem]"
+                  />
+                </div>
                 <img
-                  src={share}
-                  alt="share icon"
-                  className="h-[3.125rem] w-[1.875rem] relative -bottom-[0.85rem] -right-[0.85rem]"
+                  className="absolute right-[3.5%] top-[3%] cursor-pointer"
+                  src={cross}
+                  alt="check icon"
                 />
-              </div>
-              <img
-                className="absolute right-[3.5%] top-[3%] cursor-pointer"
-                src={cross}
-                alt="check icon"
-              />
-            </section>
-          </div>
+              </section>
+            </div>
+          ))
         ) : (
           <div>No notifications yet</div>
         )}
