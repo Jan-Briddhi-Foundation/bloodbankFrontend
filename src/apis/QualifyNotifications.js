7db4876bebe710860a9e4e2a0093d673cfac44e9
dotjarvis <johnsonmakings@gmail.com>
2024-02-02 12:59:08 +0300

    ThankYou, YourInfo & QuaificationNotifications confirmed working

--- src/apis/QualifyNotifications.js
+++ src/apis/QualifyNotifications.js
@@ -1,17 +1,17 @@
 import axios from "axios";
-const backendURL =  "https://bloodbak.onrender.com";
+const backendURL = "https://bloodbak.onrender.com";
 
 export const qualifyDonor = async (data) => {
   try {
     const requrl = `${backendURL}/api/donation-criteria/`;
     const token = JSON.parse(localStorage.getItem("bloodBankAuthToken"));
-    console.log(token);
+
     const config = {
       headers: {
         Authorization: token,
       },
     };
-    const response = await axios.post(requrl, data , config, );
+    const response = await axios.post(requrl, data, config);
     return response.data;
   } catch (error) {
     if (error) {
