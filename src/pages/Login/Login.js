b0cf0820be8e665760cc1d28d0f7ded365f68b2e
dotjarvis <johnsonmakings@gmail.com>
2024-02-17 00:15:36 +0300

    Tailwind in Login complete

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -2,9 +2,9 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
 import { forgotPassword, login } from "../../apis/Auth";
-// import apple from "../../assets/Apple.png";
-// import facebook from "../../assets/Facebook.svg";
-// import google from "../../assets/Google.svg";
+import apple from "../../assets/Apple.png";
+import facebook from "../../assets/Facebook.svg";
+import google from "../../assets/Google.svg";
 import email from "../../assets/email.svg";
 import loginLeft from "../../assets/loginLeft.png";
 import loginRight from "../../assets/loginRight.png";
@@ -14,6 +14,13 @@ import { CheckAuth } from "../../apis/LoggedInProfileType";
 
 const Login = () => {
   (async () => await CheckAuth())();
+
+  const formfield =
+    "grid grid-cols-[auto,1fr] rounded overflow-hidden bg-[#f9f9f9] focus:outline mb-6";
+  const inputformField =
+    "bg-[#f9f9f9] p-2 text-3xl focus:outline-none max-[410px]:text-2xl max-[410px]:p-0";
+  const iconStyle =
+    "w-16 m-5 pr-2.5 border-r-[1px] border-black max-[410px]:w-12 ";
 
   const redirect = useNavigate();
   const [user, setUser] = useState({
@@ -84,80 +91,97 @@ const Login = () => {
   return (
     <>
       <Header />
-      <div className="md:h-screen md:flex md:gap-8 py-4 justify-between items-start mt-4 w-[auto] px-4 mb-8">
-        <img
-          className=" flex w-[15rem] h-[auto] md:self-end  md:ml-8 md:py-16 py-8"
-          src={loginLeft}
-          alt="medicine"
-        />
-        <main className=" bg-white  md:py-2 px-4 flex flex-col  gap-4 items-center w-[auto] rounded-[5px] border-2 border-[#BA595F]">
-          <h1 className="text-black text-2xl md:text-3xl p-4">Welcome!</h1>
-          <h1 className="text-black text-2xl md:text-2xl">sign in</h1>
-          <section className="flex flex-col gap-6 items-center">
-            <div className="flex items-center w-[auto] bg-white rounded-[5px] gap-2 border p-1">
-              <img className="w-[2rem]" src={email} alt="emailicon" />
-              <div className="h-[5vh] w-px bg-black self-center"></div>
-              <input
-                type="text"
-                name="email"
-                placeholder="E-mail*"
-                value={user.email}
-                className="w-[100%] md:w-[24rem] gap-2 text-xs"
-                onChange={(e) => {
-                  setUser({ ...user, email: e.target.value });
-                }}
-              />
-            </div>
-            <div className="flex items-center w-[auto]  bg-white rounded-[5px] gap-2 border p-1">
-              <img className="w-[2rem]" src={password} alt="passwordicon" />
-              <div className="h-[5vh] w-px  bg-black self-center"></div>
-              <input
-                type="password"
-                name="password"
-                placeholder="Password*"
-                required
-                className="w-[15rem] md:w-[24rem] gap-2 text-lg"
-                value={user.password}
-                onChange={(e) => {
-                  setUser({ ...user, password: e.target.value });
-                }}
-              />
-            </div>
-            <a
-              className="self-start"
-              href="#forgot"
-              onClick={handleForgotPassword}
-            >
-              Forgot password
-            </a>
 
-            <div className="flex gap-4 w-[100%] justify-around">
-              <button
-                className="bg-[#BA595F] border-[1px]  border-[#BA595F] rounded-[5px] font-[Roboto] text-[1rem] font-medium  tracking-normal text-center text-[rgba(255,_255,_255,_1)] w-[9rem] md:w-[13rem] max-w-[16rem] h-[auto] p-2 cursor-pointer"
-                onClick={handleSumbit}
-              >
-                Login
-              </button>
-              <button
-                className=" border-[1px]  border-[#BA595F] rounded-[5px] font-[Roboto] text-[1rem] font-medium  tracking-normal text-center text-[#BA595F] h-[auto]  p-2  cursor-pointer w-[9rem] md:w-[13rem] max-w-[16rem]"
-                onClick={() => redirect("/register1")}
-              >
-                Register
-              </button>
+      <section className="px-11 max-[410px]:px-6">
+        <div className="my-16 grid grid-cols-[auto,1fr,auto] gap-x-2">
+          <img className="self-end max-w-sm" src={loginLeft} alt="medicine" />
+          <div className="rounded-lg border-2 border-[#BA595F]">
+            <div className="mb-12 text-center">
+              <h1 className="text-6xl text-[#222222] mb-6 font-bold max-[410px]:text-5xl">
+                Welcome!
+              </h1>
+              <h2 className="text-[#222222] text-5xl font-semibold mb-12 max-[410px]:text-4xl">
+                sign in
+              </h2>
             </div>
-          </section>
-          <div className="">
-            <div></div>
-            Or continue with
-            <div></div>
+            <form className="flex flex-col gap-y-4 mb-24">
+              <div className={formfield}>
+                <img className={iconStyle} src={email} alt="email-icon" />
+
+                <input
+                  className={inputformField}
+                  type="text"
+                  name="email"
+                  placeholder="E-mail*"
+                  value={user.email}
+                  required
+                  onChange={(e) => setUser({ ...user, email: e.target.value })}
+                />
+              </div>
+
+              <div className={formfield}>
+                <img className={iconStyle} src={password} alt="password-icon" />
+
+                <input
+                  className={inputformField}
+                  type="password"
+                  name="password"
+                  placeholder="Password*"
+                  required
+                  value={user.password}
+                  onChange={(e) =>
+                    setUser({ ...user, password: e.target.value })
+                  }
+                />
+              </div>
+              <a
+                className="self-start text-3xl underline underline-offset-2 mt-6 mb-8"
+                href="#forgot"
+                onClick={handleForgotPassword}
+              >
+                Forgot password
+              </a>
+
+              <div className="grid grid-cols-2 gap-12 justify-around">
+                <button
+                  className="bg-[#ba595f] border-[1px] border-[#ba595f] rounded-[5px] font-[Roboto] text-3xl p-3 uppercase font-medium tracking-normal text-center text-[#f9f9f9] font-medium cursor-pointer"
+                  onClick={handleSumbit}
+                >
+                  Login
+                </button>
+                <button
+                  className="border-[1px] border-[#ba595f] rounded-[5px] font-[Roboto] text-3xl p-3 uppercase font-medium tracking-normal text-center text-[#ba595f] font-medium cursor-pointer"
+                  onClick={() => redirect("/register1")}
+                >
+                  Register
+                </button>
+              </div>
+            </form>
+            <div className="">
+              <p className="text-2xl">Or continue with</p>
+            </div>
+            <div className="flex gap-24 mb-4 ">
+              <p className="h-[3rem] w-[3rem] flex flex-col gap-1 items-center">
+                <img src={google} alt="google" />
+                <span className="text-xl">Google</span>
+              </p>
+              <p className="h-[3rem] w-[3rem] flex flex-col gap-1 items-center ">
+                <img src={facebook} alt="facebook" />
+                <span className="text-xl">Facebook</span>
+              </p>
+              <p className="h-[3rem] w-[3rem] flex flex-col gap-1 items-center">
+                <img src={apple} alt="apple" />
+                <span className="text-xl">Apple</span>
+              </p>
+            </div>
           </div>
-        </main>
-        <img
-          className="w-[20rem] self-start mt-4 p-4"
-          src={loginRight}
-          alt="medicine"
-        />
-      </div>
+          <img
+            className="self-start max-w-sm"
+            src={loginRight}
+            alt="medicine"
+          />
+        </div>
+      </section>
       <ToastContainer
         position="top-right"
         autoClose={5000}
